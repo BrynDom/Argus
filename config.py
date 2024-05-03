77889b03d82381324432c0dd549678203d32fa59
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-05-03 19:29:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,16 +11,16 @@ bot_token = getenv("bot_token", None)
 db_name = getenv("db_name", None)
 mongo_uri = getenv("mongo_uri", None)
 def_bahasa = getenv("def_bahasa", "toxic")
-log_pic = getenv("log_pic", "https://telegra.ph//file/43cec0ae0ded594b55247.jpg")
+log_pic = getenv("log_pic", "https://telegra.ph/file/a624296bc3dfdd3df57ea.png")
 heroku_api = getenv("heroku_api")
 heroku_app_name = getenv("heroku_app_name")
 upstream_repo = getenv(
     "upstream_repo",
-    "https://github.com/naya1503/Mix-Userbot",
+    "https://github.com/BrynDom/Argus-Bot",
 )
 upstream_branch = getenv("upstream_branch", "dev")
 git_token = getenv("git_token", None)
-alive_pic = getenv("alive_pic", "https://telegra.ph//file/43cec0ae0ded594b55247.jpg")
+alive_pic = getenv("alive_pic", "https://telegra.ph/file/a624296bc3dfdd3df57ea.png")
 log_channel = getenv("log_channel", None)
 genius_api = getenv(
     "genius_api",
